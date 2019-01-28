02eb9c19c00e6cb781efc44a78d3ef4d5e276669
Michael Velciov <michaelvelciov@hotmail.com>
2019-01-28 09:19:47 -0500

    CV FINI

--- CV.java
+++ CV.java
@@ -5,17 +5,37 @@ public class CV {
 	
 	String nom, prenom, formation, attente;
 	int expTravail;
-	ArrayList<String> competences = new ArrayList<String>();
+	String[] competences;
 	
+	
+	public CV(String nom, String prenom, String formation, String attente, int expTravail,
+			String... competences) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.attente = attente;
+		this.expTravail = expTravail;
+		this.competences = competences;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Bienvenue chez Barette!");
+		ArrayList<CV> cvTable = new ArrayList<CV>();
+		cvTable.add(new CV("Valiquette", "Yanick", "Cegep Montmorency", "Aucune attentes", 15, "Java PROFESSIONAL" )) ;
+		cvTable.add(new CV("Velciov", "Michael", "Cegep Montmorency", "Aucune attentes", 16, "Java Expert"));
+		cvTable.forEach(cv -> {
+			cv.affiche();
+		});
+	}
 	
 	public void affiche() {
-		System.out.println("Nom : " + nom);
+		System.out.println("\nNom : " + nom);
 		System.out.println("Prenom : " + prenom);
 		System.out.println("Formation : " + formation);
 		System.out.println("Experiences de travail : " + expTravail);
-		competences.forEach(competence -> {
+		for (String competence : competences) {
 			System.out.println("Competence : " + competence);
-		});
+		}
 		System.out.println("Attentes : " + attente);
 	}
 	
