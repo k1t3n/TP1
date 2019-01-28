bc4e915dd046a54b14ca58f8a41fc4fd32440052
Michael Velciov <michaelvelciov@hotmail.com>
2019-01-28 09:06:59 -0500

    Creation Fichier Java

---
+++ CV.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class CV {
+
+	
+	String nom, prenom, formation, attente;
+	int expTravail;
+	ArrayList<String> competences = new ArrayList<String>();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
