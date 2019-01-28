1aa2466b62791832aa384201b535db84d48c17f8
Michael Velciov <michaelvelciov@hotmail.com>
2019-01-28 09:09:22 -0500

    Methode afficher Java

--- CV.java
+++ CV.java
@@ -8,6 +8,16 @@ public class CV {
 	ArrayList<String> competences = new ArrayList<String>();
 	
 	
+	public void affiche() {
+		System.out.println("Nom : " + nom);
+		System.out.println("Prenom : " + prenom);
+		System.out.println("Formation : " + formation);
+		System.out.println("Experiences de travail : " + expTravail);
+		competences.forEach(competence -> {
+			System.out.println("Competence : " + competence);
+		});
+		System.out.println("Attentes : " + attente);
+	}
 	
 	
 	
